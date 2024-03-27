e6be9f75aae91936d243f012e788b93573b42682
tanmoysarkershuvo <tanmoysarkershuvo2@gmail.com>
2024-03-27 20:24:00 +0600

    init commit

---
+++ DataType.py
@@ -0,0 +1,22 @@
+#string type data
+name = "Tanmoy Sarker Shuvo"
+print(name)
+
+# int type data
+Tanmoy = 777
+print(type(Tanmoy))
+
+# floting type data
+Sarker = 509999999999.2
+print(type(Sarker))
+
+#complex type data
+Shuvo = 776666666666657j
+print(type(Shuvo))
+
+#str type data
+YourName="Tanmoy"
+MyName="Shuvo"
+print(YourName + MyName)
+print("My Name is" + " " + MyName)
+
